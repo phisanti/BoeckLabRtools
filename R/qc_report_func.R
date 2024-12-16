26ab6778eb1091ed3e1b9aaf9626ebe4eb453195
tisalon <tisalon@outlook.com>
2024-12-16 15:43:57 +0100

    use theme_plateview

--- R/qc_report_func.R
+++ R/qc_report_func.R
@@ -7,35 +7,28 @@
 #'
 #' @return A grid of heatmaps showing the object class distribution at t0.
 create_object_class_plots <- function(d) {
-    total_count <- object_class <- NULL
+  . <- frame <- total_count <- object_class <- NULL
   d_summary <- d[frame == 0, 
             .(count = sum(total_count)), 
             by = .(row, col, object_class)]
   object_classes <- unique(d_summary$object_class)
   
-  plot_list <- purrr::map(object_classes, ~{
-    ggplot2::ggplot(d_summary[object_class == .x], ggplot2::aes(x = col, y = row, fill = count)) +
-      ggplot2::geom_tile() +
-      ggplot2::scale_fill_distiller(palette = "Spectral", direction = -1) +
-      ggplot2::scale_y_discrete(limits = rev(levels(factor(d_summary$row)))) +
-      ggplot2::scale_x_continuous(breaks = 1:max(d_summary$col), labels = 1:max(d_summary$col)) +
-      ggplot2::theme_void() +
-      ggplot2::labs(x = "Column", y = "Row", fill = "Count", title = .x) +
-      ggplot2::theme(
-        axis.text.x = ggplot2::element_text(angle = 90, hjust = 1, vjust = 0.5, size = 6),
-        axis.text.y = ggplot2::element_text(size = 6),
-        axis.title = ggplot2::element_text(size = 10)
-      )
-  })
+  plot_list <- lapply(object_classes, function(.x) {
+  ggplot2::ggplot(d_summary[object_class == .x], ggplot2::aes(x = col, y = row, fill = count)) +
+    ggplot2::geom_tile() +
+    ggplot2::scale_fill_distiller(palette = "Spectral", direction = -1) +
+    theme_plateview(col_var = d_summary$col, row_var = d_summary$row,
+                    fill_lab = "Count", title = .x)
+                    })
   
-    grid_plot <- gridExtra::grid.arrange(
+  grid_plot <- suppressMessages(gridExtra::grid.arrange(
     plot_list[[1]], plot_list[[2]],
     plot_list[[3]], plot_list[[4]],
     plot_list[[5]],
     ncol = 2,
     top = grid::textGrob(expression(paste("Object Classes Distribution at t"[0])), 
                     gp = grid::gpar(fontsize = 16, font = 2))
-    )
+  ))
   return(grid_plot)
 }
 
@@ -62,8 +55,6 @@ create_growth_plot <- function(d) {
   ggplot2::ggplot(growth_ratio, ggplot2::aes(x = col, y = row, fill = log(growth_ratio))) +
     ggplot2::geom_tile() +
     viridis::scale_fill_viridis(option = "plasma", direction = -1) +
-    ggplot2::scale_y_discrete(limits = rev(levels(factor(growth_ratio$row)))) +
-    ggplot2::scale_x_continuous(breaks = 1:max(growth_ratio$col), labels = 1:max(growth_ratio$col)) +
     ggplot2::theme_void() +
     theme_plateview(col_var = growth_ratio$col, row_var = growth_ratio$row,
                   fill_lab = expression(Log(Area[t[0]] / Area[t[max]])), title = "Growth Approximation")
@@ -98,23 +89,29 @@ create_consistency_plot <- function(d) {
   ggplot2::ggplot(classification_consistency, ggplot2::aes(x = col, y = row, fill = percent_change_capped)) +
     ggplot2::geom_tile() +
     ggplot2::scale_fill_distiller(palette = "Spectral", direction = 1) +
-    ggplot2::scale_y_discrete(limits = rev(levels(factor(classification_consistency$row)))) +
-    ggplot2::scale_x_continuous(breaks = 1:max(classification_consistency$col), labels = 1:max(classification_consistency$col)) +
     ggplot2::facet_wrap(~ object_class, ncol = 2) +
-    ggplot2::theme_void() +
-    ggplot2::labs(x = "Column", y = "Row", fill = "% Change/Frame", 
-         title = "Classification Consistency (% Change per Frame)",
-         subtitle = sprintf("Color scale capped at 5th (%.2f) and 95th (%.2f) percentiles", q5_q95[1], q5_q95[2])) +
-    ggplot2::theme(
-      axis.text.x = ggplot2::element_text(angle = 90, hjust = 1, vjust = 0.5, size = 6),
-      axis.text.y = ggplot2::element_text(size = 6),
-      axis.title = ggplot2::element_text(size = 10),
-      strip.text = ggplot2::element_text(size = 12, face = "bold")
+    theme_plateview(
+      col_var = classification_consistency$col,
+      row_var = classification_consistency$row,
+      col_lab = "Column",
+      row_lab = "Row",
+      fill_lab = "% Change/Frame",
+      title = "Classification Consistency (% Change per Frame)",
+      subtitle = sprintf("Color scale capped at 5th (%.2f) and 95th (%.2f) percentiles", q5_q95[1], q5_q95[2])
     )
 }
 
+#' Create PI Consistency Plot
+#'
+#' This function generates a heatmap visualizing the consistency of Propidium Iodide (PI) classification across different wells and time points, focusing on single-cell objects.
+#' For each well, frame, and object class, the fraction of PI-positive cells is calculated. The complement of the PI fraction represents the live cell fraction.
+#' The standard deviation of the live fraction across all time points is computed for each well, serving as a measure of classification consistency over time.
+#'
+#' @param d A data.table containing the data to be plotted. It should have columns 'file', 'well', 'frame', 'object_class', 'pi_class_pos', 'total_count', 'row', and 'col'.
+#'
+#' @return A heatmap showing the PI classification consistency across the plate.
 create_pi_consistency_plot <- function(d) {
-    pi_frac <- total_count <- pi_class_pos <- object_class <- live_frac <- sd_live_frac <- total_sd <- well <-  NULL
+  .SD <- . <- pi_frac <- total_count <- pi_class_pos <- object_class <- live_frac <- sd_live_frac <- total_sd <- well <-  NULL
   d[, pi_frac := sum(unique(pi_class_pos))/sum(unique(total_count)), 
     by = .(file, well, frame, object_class)]
   d[, live_frac := 1 - pi_frac]
@@ -125,18 +122,16 @@ create_pi_consistency_plot <- function(d) {
     by = .(well, row, col, frame)][, .(total_sd = sum(sd_live_frac)),
       by = .(well, row, col)] 
 
-    ggplot2::ggplot(d_susmmary, ggplot2::aes(x = col, y = row, fill = total_sd)) +
+  ggplot2::ggplot(d_summary, ggplot2::aes(x = col, y = row, fill = total_sd)) +
     ggplot2::geom_tile() +
     viridis::scale_fill_viridis(option = "plasma", direction = -1) +
-    ggplot2::scale_y_discrete(limits = rev(levels(factor(d_susmmary$row)))) +
-    ggplot2::scale_x_continuous(breaks = 1:max(d_susmmary$col), labels = 1:max(d_susmmary$col)) +
-    ggplot2::theme_void() +
-    ggplot2::labs(x = "Column", y = "Row", fill = expression(sum(sigma[t], t == 1, t[max])), title = "PI Classification Consistency (Standard Deviation)") +
-    ggplot2::theme(
-      axis.text.x = ggplot2::element_text(angle = 90, hjust = 1, vjust = 0.5),
-      axis.text.y = ggplot2::element_text(size = 8),
-      axis.title = ggplot2::element_text(size = 10),
-      strip.text = ggplot2::element_text(size = 12, face = "bold")
+    theme_plateview(
+      col_var = d_summary$col,
+      row_var = d_summary$row,
+      col_lab = "Column",
+      row_lab = "Row",
+      fill_lab = expression(sum(sigma[t], t == 1, t[max])),
+      title = "PI Classification Consistency (Standard Deviation)"
     )
 }
 
@@ -160,16 +155,14 @@ create_time_delay_plot <- function(d) {
   }, by = .(well, row, col)]
   
   ggplot2::ggplot(time_delay, ggplot2::aes(x = col, y = row, fill = delay_hours)) +
-    ggplot2::geom_tile() +
+    ggplot2::geom_tile(colour = 'lightgrey') +
     viridis::scale_fill_viridis(option = "plasma", direction = 1) +
-    ggplot2::scale_y_discrete(limits = rev(levels(factor(time_delay$row)))) +
-    ggplot2::scale_x_continuous(breaks = 1:max(time_delay$col), labels = 1:max(time_delay$col)) +
-    ggplot2::theme_void() +
-    ggplot2::labs(x = "Column", y = "Row", fill = "Delay (hours)", title = "Start Recording Time Delay") +
-    ggplot2::theme(
-      axis.text.x = ggplot2::element_text(angle = 90, hjust = 1, vjust = 0.5),
-      axis.text.y = ggplot2::element_text(size = 8),
-      axis.title = ggplot2::element_text(size = 10),
-      strip.text = ggplot2::element_text(size = 12, face = "bold")
+    theme_plateview(
+      col_var = time_delay$col,
+      row_var = time_delay$row,
+      col_lab = "Column",
+      row_lab = "Row",
+      fill_lab = "Delay (hours)",
+      title = "Start Recording Time Delay"
     )
 }
