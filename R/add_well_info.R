7de9daccd335511909eca29b5710e2f65698bb7a
tisalon <tisalon@outlook.com>
2024-12-18 14:17:12 +0100

    Add option to modify data inplace and name well information with argument

--- R/add_well_info.R
+++ R/add_well_info.R
@@ -4,9 +4,12 @@
 #'
 #' @param d A data.table containing the well information. It must have columns named "row", "col", and "well" (all lowercase).
 #' @param well_info A data.frame or data.table containing the well information. The format depends on the `plate_format` argument.
+#' @param info_name A character string specifying the name of the column containing the well information. Default is "well_info".
 #' @param plate_format A character string specifying the format of the well information. Can be either "plate_view" (default) or "transposed".
 #' @param wells_to_ignore A character vector specifying the wells to ignore. Default is c("EMPTY", "BW").
 #' @param force_lower A logical value indicating whether to force column names to lowercase. Default is TRUE.
+#' @param inplace A logical value indicating whether to modify the input data.table in place. Default is TRUE. This means the data.table given 
+#' as input will be directly modified without using the assignment operator '<-'.
 #' 
 #' @return A data.table with the merged drug data and well information.
 #'
@@ -43,7 +46,8 @@
 #' merged_data_transposed <- merge_plate_data(d, well_info_transposed, plate_format = "transposed")
 #' 
 #' @export
-merge_plate_data <- function(d, well_info, plate_format = "plate_view", wells_to_ignore = c("EMPTY", "BW"), force_lower = TRUE) {
+merge_plate_data <- function(d, well_info, info_name = 'well_info', plate_format = "plate_view", wells_to_ignore = c("EMPTY", "BW"), force_lower = TRUE, inplace = TRUE) {
+  well <- NULL
   # Check if well_info is a path to a CSV file
   if (is.character(well_info) && length(well_info) == 1 && file.exists(well_info)) {
     well_info <- data.table::fread(well_info, header = TRUE)
@@ -59,7 +63,7 @@ merge_plate_data <- function(d, well_info, plate_format = "plate_view", wells_to
         stop("Column 1 must be named 'row' in plate_view format")
       }
     }
-    well_info <- data.table::melt(well_info, id.vars = "row", variable.name = "col", value.name = "info")
+    well_info <- data.table::melt(well_info, id.vars = "row", variable.name = "col", value.name = info_name)
   } else if (plate_format == "transposed") {
     if (!all(colnames(well_info)[1:2] %in% c("col", "row"))) {
       if (force_lower) {
@@ -80,11 +84,18 @@ merge_plate_data <- function(d, well_info, plate_format = "plate_view", wells_to
       stop("Column names in d must be lowercase and include 'row', 'col' and 'well'")
     }
   }
-  
+
   # Add info and merge datasets
   well_info[, ":="(well = paste0(row, col), col = as.integer(col))]
-  well_info <- well_info[!info %in% wells_to_ignore]
-  data.table::setorder(well_info, well)
+  filt_rows <- !well_info[[info_name]] %in% wells_to_ignore
+  well_info <- well_info[filt_rows]
+  data.table::setorder(well_info, well, col, row)
   data.table::setorder(d, well)
-  d[well_info, on = c("well", "col", "row")]
+
+  if (inplace) {
+    d[well_info, (info_name) := get(info_name), on = c("well", "col", "row")]
+    return(invisible(d))
+  } else {
+    return(well_info[, .SD, .SDcols = c("well", info_name)])
+  }
 }
